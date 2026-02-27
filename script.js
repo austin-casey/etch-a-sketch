410c5fcb44a97ec21bd901304859062ee44ce7c5
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-26 16:18:32 -0900

    Completed etch-a-sketch

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 
 let numSquares = 16;
-let grid = numSquares * numSquares;
-const container = document.querySelector(".container");
+const container = document.querySelector(".grid-container");
+const btnChange = document.querySelector(".btnChange");
+const btnClear = document.querySelector(".btnClear");
 
 function createGrid() {
-    for (i = 0; i < grid; i++) {
+    const dimension = 480 / numSquares;
+    for (let i = 0; i < numSquares * numSquares; i++) {
         let square = document.createElement("div");
+        square.classList.add("gridSquare");
+        square.style.width = `${dimension}px`
+        square.style.height = `${dimension}px`
         container.appendChild(square);
     }
 }
 
 createGrid();
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("gridSquare")) {
+        e.target.style.backgroundColor = "blue";
+    }
+    
+})
+
+btnClear.addEventListener("click", (e) => {
+    const allSquares = document.querySelectorAll(".gridSquare");
+    for (const square of allSquares) { 
+        square.style.backgroundColor = "white";
+    }
+})
+
+btnChange.addEventListener("click", (e) => {
+    const newSize = prompt("Enter new grid size");
+    if (newSize > 99) {
+        alert("Your number must be less than 100.")
+    } else {
+        numSquares = Number(newSize);
+        grid = numSquares * numSquares;
+        container.innerHTML = "";
+        createGrid();
+    }
+    
+} )
